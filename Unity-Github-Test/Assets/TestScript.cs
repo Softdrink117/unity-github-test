907be891583b72762ecf55e93b99e4edb59352ac
Softdrink117 <tobyhein@gmail.com>
2017-02-07 13:00:31 -0800

    Added main Scene and TestScript C# Script

---
+++ Unity-Github-Test/Assets/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Softdrink{
+
+	public class TestScript : MonoBehaviour {
+
+		// Woooooo testing github stuff~
+
+		private Transform self;
+
+		// Use this for initialization
+		void Start () {
+			self = gameObject.GetComponent(typeof(Transform)) as Transform;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			self.position = new Vector3 (self.position.x + 0.1f, self.position.y, self.position.z);
+		}
+	}
+
+}
